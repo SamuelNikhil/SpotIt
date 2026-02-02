ec370e15b6d565509081d9ef94e3f61760c0a5f3
Your Name <your-email@example.com>
2026-02-02 20:01:02 +0530

    V1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +7,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-  }
-})
+    allowedHosts: true,
+  },
+  preview: {
+    allowedHosts: true,
+  },
+});
